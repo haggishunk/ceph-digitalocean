0138abeb265576979c56a9cb7e158e207d345b0f
Travis Mattera <haggishunk@hotmail.com>
2018-01-09 15:44:08 -0800

    Cleaned out old & unused

--- variables.tf
+++ variables.tf
@@ -13,6 +13,7 @@ variable "prefix" {
 }
 
 variable "do_region" {
+    # for this project you will want a region with volumes available
     type = "string"
     description = "DigitalOcean droplet region"
     default = "sfo2"
@@ -27,7 +28,7 @@ variable "size" {
 variable "ssh_id" {
     # change this
     type = "string"
-    description = "SSH public key ID - MD5 hash works - `ssh-keygen -l -E md5 -f ~/.ssh/id_rsa`"
+    description = "SSH public key ID - MD5 hash works: `ssh-keygen -l -E md5 -f ~/.ssh/id_rsa`"
     default = "xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
 }
 
@@ -37,34 +38,3 @@ variable "node_user" {
     default = "tentacle"
 }
 
-# GCP vars
-
-variable "gcp_dns_zone" {
-    # change this
-    type = "string"
-    description = "GCP DNS managed zone name"
-    default = "yourzone"
-}
-
-variable "gcp_region" {
-    type = "string"
-    description = "GCP VM region selection"
-    default = "us-west1"
-}
-
-
-# Shared/misc  vars
-variable "domain" {
-    # change this
-    type = "string"
-    description = "The base domain"
-    default = "domain.tld"
-}
-
-variable "email" {
-    # change this
-    # note: unicode \u003C and \u003Efor angle brackets
-    type = "string"
-    description = "Email address for CSR to Let's Encrypt"
-    default = "\u003Cperson@domain.tld\u003E"
-}
