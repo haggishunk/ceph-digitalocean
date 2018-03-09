eb8e93205b0b84ee23a4b2a3d5c094631a885a94
Travis Mattera <haggishunk@hotmail.com>
2018-03-09 10:08:07 -0800

    Format

--- volumes.tf
+++ volumes.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_volume" "ceph-vol" {
-    region                  = "${var.region}"
-    count                   = "${var.worker_qty}"
-    name                    = "${var.prefix}-vol-${count.index}"
-    size                    = "${var.size_vol}"
+  region = "${var.region}"
+  count  = "${var.worker_qty}"
+  name   = "${var.prefix}-vol-${count.index}"
+  size   = "${var.size_vol}"
 }
