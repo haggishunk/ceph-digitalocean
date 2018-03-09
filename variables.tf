a1941ddf45bc463c6f6e69170ee5e8d4e4b40dad
Travis Mattera <haggishunk@hotmail.com>
2018-03-09 10:47:15 -0800

    Added ssh admin source cidr; formatted

--- variables.tf
+++ variables.tf
@@ -1,57 +1,57 @@
 # DigitalOcean vars
 
-variable "admin_image" {
-    type        ="string"
-    description = "Admin image slug"
-    default     = "ubuntu-14-04-x64"
-}
-
-variable "node_image" {
-    type        = "string"
-    description = "Image name"
-    default     = "ubuntu-14-04-x64"
+variable "image" {
+  type        = "string"
+  description = "Admin image slug"
+  default     = "ubuntu-14-04-x64"
 }
 
 variable "worker_qty" {
-    type        = "string"
-    description = "Number of droplets to deploy"
-    default     = "1"
+  type        = "string"
+  description = "Number of droplets to deploy"
+  default     = "1"
 }
 
 variable "prefix" {
-    type        = "string"
-    description = "Basename of droplets"
-    default     = "whateveryoulike"
+  type        = "string"
+  description = "Basename of droplets"
+  default     = "whateveryoulike"
 }
 
 variable "region" {
-    # for this project you will want a region with volumes available
-    type        = "string"
-    description = "DigitalOcean droplet region"
-    default     = "sfo2"
+  # for this project you will want a region with volumes available
+  type        = "string"
+  description = "DigitalOcean droplet region"
+  default     = "sfo2"
 }
 
 variable "size" {
-    type        = "string"
-    description = "Droplet RAM"
-    default     = "1GB"
+  type        = "string"
+  description = "Droplet RAM"
+  default     = "1GB"
 }
 
 variable "size_vol" {
-    type        = "string"
-    description = "Volume size in GB"
-    default     = 20
+  type        = "string"
+  description = "Volume size in GB"
+  default     = 20
 }
 
 variable "ssh_id" {
-    # change this
-    type        = "string"
-    description = "SSH public key ID - MD5 hash works: `ssh-keygen -l -E md5 -f ~/.ssh/id_rsa`"
-    default     = "xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
+  # change this
+  type        = "string"
+  description = "SSH public key ID - MD5 hash works: `ssh-keygen -l -E md5 -f ~/.ssh/id_rsa`"
+  default     = "xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
 }
 
 variable "user" {
-    type        = "string"
-    description = "User to provision on each ceph node"
-    default     = "tentacle"
+  type        = "string"
+  description = "User to provision on each ceph node"
+  default     = "tentacle"
+}
+
+variable "admin_cidr" {
+  type        = "string"
+  description = "Source CIDR block for SSH admin"
+  default     = "0.0.0.0/0"
 }
